5005a7a0888453f79aef83771cd2b49723429d91
Mohamed1197 <m.hafiz97@outlook.com>
2022-07-01 01:46:56 +0200

    NVIC Added Vector Table MACROS

--- 02-MCAL/03-NVIC/NVIC_interface.h
+++ 02-MCAL/03-NVIC/NVIC_interface.h
@@ -2,7 +2,7 @@
 /**************** Name    : Mohamed Ahmed Abdelhamid ********************/
 /**************** Date    :        30/06/2022        ********************/
 /**************** SWC     :          GPIO            ********************/
-/**************** Version :           1.0            ********************/
+/**************** Version :           1.1            ********************/
 /************************************************************************/
 
 #ifndef NVIC_INTERFACE_H
@@ -56,8 +56,67 @@ u8 MNVIC_u8GetActiveFlag (u8 Copy_u8IntNumber , u8* Copy_pu8FlagResult);
 /*#######################################################################*/
 /*                                 MACROS                                */
 /*#######################################################################*/
-
-
+/*                Vector Table ISR Interrupt ID    >PAGE(204)<           */
+#define NVIC_VEC_TBL_WWDG                  0
+#define NVIC_VEC_TBL_PVD                   1
+#define NVIC_VEC_TBL_TAMPER                2
+#define NVIC_VEC_TBL_RTC                   3
+#define NVIC_VEC_TBL_FLASH                 4
+#define NVIC_VEC_TBL_RCC                   5
+#define NVIC_VEC_TBL_EXTI0                 6
+#define NVIC_VEC_TBL_EXTI1                 7
+#define NVIC_VEC_TBL_EXTI2                 8
+#define NVIC_VEC_TBL_EXTI3                 9
+#define NVIC_VEC_TBL_EXTI4                10
+#define NVIC_VEC_TBL_DMA1_CHANNEL1        11
+#define NVIC_VEC_TBL_DMA1_CHANNEL2        12
+#define NVIC_VEC_TBL_DMA1_CHANNEL3        13
+#define NVIC_VEC_TBL_DMA1_CHANNEL4        14
+#define NVIC_VEC_TBL_DMA1_CHANNEL5        15
+#define NVIC_VEC_TBL_DMA1_CHANNEL6        16
+#define NVIC_VEC_TBL_DMA1_CHANNEL7        17
+#define NVIC_VEC_TBL_ADC1_2               18
+#define NVIC_VEC_TBL_USB_HP_CAN_TX        19
+#define NVIC_VEC_TBL_USB_LP_CAN_RX0       20
+#define NVIC_VEC_TBL_CAN_RX1              21
+#define NVIC_VEC_TBL_CAN_SCE              22
+#define NVIC_VEC_TBL_EXTI9_5              23
+#define NVIC_VEC_TBL_TIM1_BRK             24
+#define NVIC_VEC_TBL_TIM1_UP              25
+#define NVIC_VEC_TBL_TIM1_TRG_COM         26
+#define NVIC_VEC_TBL_TIM1_CC              27
+#define NVIC_VEC_TBL_TIM2                 28
+#define NVIC_VEC_TBL_TIM3                 29
+#define NVIC_VEC_TBL_TIM4                 30
+#define NVIC_VEC_TBL_I2C1_EV              31
+#define NVIC_VEC_TBL_I2C1_ER              32
+#define NVIC_VEC_TBL_I2C2_EV              33
+#define NVIC_VEC_TBL_I2C2_ER              34
+#define NVIC_VEC_TBL_SPI1                 35
+#define NVIC_VEC_TBL_SPI2                 36
+#define NVIC_VEC_TBL_USART1               37
+#define NVIC_VEC_TBL_USART2               38
+#define NVIC_VEC_TBL_USART3               39
+#define NVIC_VEC_TBL_EXTI15_10            40
+#define NVIC_VEC_TBL_RTC_ALARM            41
+#define NVIC_VEC_TBL_USB_WAKEUP           42
+#define NVIC_VEC_TBL_TIM8_BRK             43
+#define NVIC_VEC_TBL_TIM8_UP              44
+#define NVIC_VEC_TBL_TIM8_TRG_COM         45
+#define NVIC_VEC_TBL_TIM8_CC              46
+#define NVIC_VEC_TBL_ADC3                 47
+#define NVIC_VEC_TBL_FSMC                 48
+#define NVIC_VEC_TBL_SDIO                 49
+#define NVIC_VEC_TBL_TIM5                 50
+#define NVIC_VEC_TBL_SPI3                 51
+#define NVIC_VEC_TBL_UART4                52
+#define NVIC_VEC_TBL_UART5                53
+#define NVIC_VEC_TBL_TIM6                 54
+#define NVIC_VEC_TBL_TIM7                 55
+#define NVIC_VEC_TBL_DMA2_CHANNEL1        56
+#define NVIC_VEC_TBL_DMA2_CHANNEL2        57
+#define NVIC_VEC_TBL_DMA2_CHANNEL3        58
+#define NVIC_VEC_TBL_DMA2_CHANNEL4_5      59
 /*########################################################################*/
 
 
